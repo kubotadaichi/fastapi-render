f8638738dd96d137b30471d08689c7116caffa04
rat <rat.gate64@gmail.com>
2025-10-22 15:59:34 +0900

    fix duplication

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware # 追加
 
+
 app = FastAPI()
 
-# CORSを回避するために追加（今回の肝）
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -11,8 +11,6 @@ app.add_middleware(
     allow_methods=["*"],      # 追記により追加
     allow_headers=["*"]       # 追記により追加
 )
-
-app = FastAPI()
 
 @app.get("/")
 def read_root():
