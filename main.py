5b24ae7a44df61a4b1953bf47dd7bc7aee113cc7
rat <rat.gate64@gmail.com>
2025-10-22 15:50:54 +0900

    add /api/answer

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware # 追加
+
+app = FastAPI()
+
+# CORSを回避するために追加（今回の肝）
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,   # 追記により追加
+    allow_methods=["*"],      # 追記により追加
+    allow_headers=["*"]       # 追記により追加
+)
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
 	return {"message" : "Hello, Render!"}
+
+@app.post("/api/answer")
+def answer(text: str):
+	return {"text": text}
